cba67265d328523ac325aad6363aaa69a1a19717
PillerVa <vaniapiller@macbook-pro-de-vania.home>
2026-05-04 16:55:29 +0200

    Creating Product class + variables and constructor

---
+++ src/Product.java
@@ -0,0 +1,8 @@
+public class Product {
+    private String name;
+    private double price;
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
