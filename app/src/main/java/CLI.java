35682b8065188c07d6ff8e95fb98f2b7da9316d0
PillerVa <vaniapiller@macbook-pro-de-vania.home>
2026-05-13 16:16:45 +0200

    Formatting code to Google Java Format

--- app/src/main/java/CLI.java
+++ app/src/main/java/CLI.java
@@ -2,19 +2,18 @@ import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
 @Command(
-        name = "cli",
-        description = "Vendure command line interface",
-        subcommands = {CLI_List.class}
-)
+    name = "cli",
+    description = "Vendure command line interface",
+    subcommands = {CLI_List.class})
 public class CLI implements Runnable {
 
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new CLI()).execute(args);
-        System.exit(exitCode);
-    }
+  public static void main(String[] args) {
+    int exitCode = new CommandLine(new CLI()).execute(args);
+    System.exit(exitCode);
+  }
 
-    @Override
-    public void run() {
-        System.out.println("Use a subcommand, e.g.: list --format table");
-    }
+  @Override
+  public void run() {
+    System.out.println("Use a subcommand, e.g.: list --format table");
+  }
 }
