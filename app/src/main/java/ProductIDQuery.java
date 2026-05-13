c31c172c65c7037d9895925e40eb7ea7e6727f5f
PillerVa <vaniapiller@macbook-pro-de-vania.home>
2026-05-13 18:17:27 +0200

    Creation of ProductIDQuery, which searches for a product based on the ID given by the client.

---
+++ app/src/main/java/ProductIDQuery.java
@@ -0,0 +1,28 @@
+import org.json.JSONObject;
+
+import java.awt.*;
+
+public class ProductIDQuery implements GraphQLQuery<Product> {
+
+  private String id;
+
+  public ProductIDQuery(String id) {
+    this.id = id;
+  }
+
+  @Override
+  public String getQuery() {
+    return "{ product(id: \"" + id + "\") { name variants { priceWithTax } } }";
+  }
+
+  @Override
+  public Product parseResponse(String responseBody) {
+    JSONObject json = new JSONObject(responseBody);
+    JSONObject product = json.getJSONObject("data").getJSONObject("product");
+
+    String name = product.getString("name");
+    double price = product.getJSONArray("variants").getJSONObject(0).getDouble("priceWithTax");
+
+    return new Product(name, price);
+  }
+}
